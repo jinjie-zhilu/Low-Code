2388e7378e64f6dfac808b39348f3e5b9572c46f
Richy_D <1245413737@qq.com>
2022-08-19 03:02:32 +0800

    深层拷贝
    添加深层拷贝功能

---
+++ src/utils/deepcopy.ts
@@ -0,0 +1,33 @@
+export function deepcopy(obj: Object): Object {
+    if (obj instanceof Object) {
+        const newObj: Object = {}
+        if (Array.isArray(obj)) {
+            const arr: Array<Object> = []
+            obj.forEach(item => {
+                arr.push(deepcopy(item))
+            })
+            return arr
+        } else {
+            for (const key in obj) {
+                const value: Object = obj[key]
+                if (typeof value === 'function') {
+                    newObj[key] = value.bind(newObj);
+                } else if (typeof value === 'object') {
+                    if (Array.isArray(value)) {
+                        newObj[key] = [];
+                        value.forEach(item => {
+                            newObj[key].push(deepcopy(item));
+                        });
+                    } else {
+                        newObj[key] = deepcopy(value);
+                    }
+                } else {
+                    newObj[key] = value;
+                }
+            }
+        }
+        return newObj;
+    } else {
+        return obj;
+    }
+}
