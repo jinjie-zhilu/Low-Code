957009d0510b893544550d54e942ce49f5390035
Richy_D <1245413737@qq.com>
2022-08-20 17:48:10 +0800

    修复“使用属性编辑栏修改坐标再拖动后组件位置异常”的问题
    属性编辑栏使用 el-input 会把 number 数据改成 string 导致出现 bug，现已修改。

--- src/components/ConfigMenu/ConfigMenu.tsx
+++ src/components/ConfigMenu/ConfigMenu.tsx
@@ -1,9 +1,11 @@
 import { useCanvasStore, useElementsStore } from "@/store"
-import { ElCollapse, ElCollapseItem, ElColorPicker, ElForm, ElFormItem, ElInput } from "element-plus"
+import { ElCollapse, ElCollapseItem, ElColorPicker, ElForm, ElFormItem, ElInput, ElInputNumber } from "element-plus"
 import { ref, defineComponent } from "vue"
 import type { Ref } from "vue"
 import { computed } from "vue"
 import type { CanvasStore, ElementsStore } from "@/interface"
+import emitter from "@/utils/bus"
+import "./ConfigMenu.css"
 
 export default defineComponent({
     setup() {
@@ -21,12 +23,17 @@ export default defineComponent({
                 let focus = elements.focusElements.focus[0]
                 
                 focusId.value = focus.id
-                baseConfigMenu.general.title = `[${focus.key}]${focus.label}-样式`
+                baseConfigMenu.general.title = `[${focus.key}]-样式`
                 return 'general'
             } else {
                 return 'canvas'
             }
         })
+
+        // 表单更新
+        const update = (value) => {
+            
+        }
         
         // 基础属性配置表单
         const baseConfigMenu = {
@@ -60,47 +67,83 @@ export default defineComponent({
             general: {
                 title: '',
                 form:
-                    <ElForm label-position="left" label-width="100px" model={elements.elements[focusId.value]} style="max-width: 100%">
+                    <ElForm
+                        label-position="left"
+                        label-width="100px"
+                        model={elements.elements[focusId.value]}
+                        style="max-width: 100%"
+                    >
                         <ElFormItem label="X坐标">
-                            <ElInput
-                                v-model={elements.elements[focusId.value].left}
-                                v-slots={{ suffix: () => 'px' }}
-                            />
+                            <div class="input-number">
+                                <ElInputNumber
+                                    v-model={elements.elements[focusId.value].left}
+                                    controlsPosition='right'
+                                    precision={0}
+                                    onChange={update}
+                                />
+                                <span class='unit'>px</span>
+                            </div>
                         </ElFormItem>
                         <ElFormItem label="Y坐标">
-                            <ElInput
-                                v-model={elements.elements[focusId.value].top}
-                                v-slots={{ suffix: () => 'px' }}
-                            />
+                            <div class="input-number">
+                                <ElInputNumber
+                                    v-model={elements.elements[focusId.value].top}
+                                    controlsPosition='right'
+                                    precision={0}
+                                    onChange={update}
+                                />
+                                <span class='unit'>px</span>
+                            </div>
                         </ElFormItem>
                         <ElFormItem label="文本内容">
                             <ElInput
                                 v-model={elements.elements[focusId.value].label}
                                 type="textarea"
+                                onChange={update}
                             />
                         </ElFormItem>
                         <ElFormItem label="控件宽度">
-                            <ElInput
-                                v-model={elements.elements[focusId.value].width}
-                                v-slots={{ suffix: () => 'px' }}
-                            />
+                            <div class="input-number">
+                                <ElInputNumber
+                                    v-model={elements.elements[focusId.value].width}
+                                    controlsPosition='right'
+                                    precision={0}
+                                    onChange={update}
+                                />
+                                <span class='unit'>px</span>
+                            </div>
                         </ElFormItem>
                         <ElFormItem label="控件高度">
-                            <ElInput
-                                v-model={elements.elements[focusId.value].height}
-                                v-slots={{ suffix: () => 'px' }}
-                            />
+                            <div class="input-number">
+                                <ElInputNumber
+                                    v-model={elements.elements[focusId.value].height}
+                                    controlsPosition='right'
+                                    precision={0}
+                                    onChange={update}
+                                />
+                                <span class='unit'>px</span>
+                            </div>
                         </ElFormItem>
                         <ElFormItem label="叠放层级">
-                            <ElInput
-                                v-model={elements.elements[focusId.value].zIndex}
-                            />
+                            <div class="input-number">
+                                <ElInputNumber
+                                    v-model={elements.elements[focusId.value].zIndex}
+                                    controlsPosition='right'
+                                    precision={0}
+                                    onChange={update}
+                                />
+                            </div>
                         </ElFormItem>
                         <ElFormItem label="字号">
-                            <ElInput
-                                v-model={elements.elements[focusId.value].fontSize}
-                                v-slots={{ suffix: () => 'px' }}
-                            />
+                            <div class="input-number">
+                                <ElInputNumber
+                                    v-model={elements.elements[focusId.value].fontSize}
+                                    controlsPosition='right'
+                                    precision={0}
+                                    onChange={update}
+                                />
+                                <span class='unit'>px</span>
+                            </div>
                         </ElFormItem>
                         <ElFormItem label="element">
                             <ElColorPicker v-model={elements.elements[focusId.value].color} />
