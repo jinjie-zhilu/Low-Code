13f4d101bb065f203adf3773ccbb73924dbec642
LengHoojj <2973504395@qq.com>
2022-08-22 11:50:11 +0800

    添加devtools配置

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,16 @@ import router from './router/index'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import './assets/css/dark.css'
 
+// main.ts 中加入如下代码
 const app = createApp(App)
+const win: any = window // 
+if (process.env.NODE_ENV === 'development') {
+  if ('__VUE_DEVTOOLS_GLOBAL_HOOK__' in win) {
+    // 这里__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue赋值一个createApp实例
+    win.__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue = app
+  }
+}
+
 setupStore(app)
 
 app
